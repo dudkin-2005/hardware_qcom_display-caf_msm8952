fa7c04c0a1bd28f9e7e71fbaf01cbec78e25448c
dudkin-2005 <dudkin-2005@yandex.ru>
2019-07-04 10:45:51 +0000

    fix build

--- libqdutils/Android.mk
+++ libqdutils/Android.mk
@@ -20,20 +20,6 @@ include $(BUILD_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 
 LOCAL_COPY_HEADERS_TO           := $(common_header_export_path)
-LOCAL_COPY_HEADERS              := DisplayConfig.h display_config.h
-LOCAL_SHARED_LIBRARIES          := $(common_libs) libbinder libqservice libqdutils
-LOCAL_C_INCLUDES                := $(common_includes)
-LOCAL_ADDITIONAL_DEPENDENCIES   := $(common_deps)
-LOCAL_SRC_FILES                 := display_config.cpp
-LOCAL_CFLAGS                    := $(common_flags)
-LOCAL_CFLAGS                    += -DLOG_TAG=\"DisplayConfig\"
-LOCAL_MODULE_TAGS               := optional
-LOCAL_MODULE                    := libdisplayconfig
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_COPY_HEADERS_TO           := $(common_header_export_path)
 LOCAL_COPY_HEADERS              := qdMetaData.h qd_utils.h
 LOCAL_SHARED_LIBRARIES          := liblog libcutils
 LOCAL_C_INCLUDES                := $(common_includes)
@@ -44,19 +30,4 @@ LOCAL_CFLAGS                    += -DLOG_TAG=\"DisplayMetaData\"
 LOCAL_MODULE_TAGS               := optional
 LOCAL_MODULE                    := libqdMetaData
 LOCAL_VENDOR_MODULE             := true
-include $(BUILD_SHARED_LIBRARY)
-
-
-include $(CLEAR_VARS)
-
-LOCAL_SHARED_LIBRARIES          := liblog libcutils
-LOCAL_C_INCLUDES                := $(common_includes)
-LOCAL_ADDITIONAL_DEPENDENCIES   := $(common_deps)
-LOCAL_SRC_FILES                 := qdMetaData.cpp qd_utils.cpp
-LOCAL_CFLAGS                    := $(common_flags) -Wno-sign-conversion
-LOCAL_CFLAGS                    += -DLOG_TAG=\"DisplayMetaData\"
-
-LOCAL_MODULE_TAGS               := optional
-LOCAL_MODULE                    := libqdMetaData.system
-
 include $(BUILD_SHARED_LIBRARY)
